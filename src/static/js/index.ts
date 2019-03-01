febe5a74e43ea4bf0799a32207fc396aeeaa71f5
guojiuling <514360255@qq.com>
2019-03-02 01:46:20 +0800

    首页

--- src/static/js/index.ts
+++ src/static/js/index.ts
@@ -5,6 +5,31 @@
  */
 import '../css/index.scss';
 
-$('img').each((key: number, item: any) => {
-    console.log(key, $(item).attr('src'));
+// π„∏Ê√ΩΩÈ
+let mediaLeftContent: any = $('.media-left-content');
+let mediaLeftTab: any = $('.media-left-tab');
+let tabIndex: number = 0;
+mediaLeftTab.on('mouseenter', '.media-left-tab-list', function() {
+   let index: number = $(this).index();
+    mediaLeftTab
+        .find('.media-left-tab-list')
+        .eq(index)
+        .addClass('current')
+        .siblings()
+        .removeClass('current');
+    mediaLeftTab
+        .find('img')
+        .eq(tabIndex)
+        .attr('src', `static/images/media-img${tabIndex + 1}-link.png`);
+    mediaLeftTab
+        .find('img')
+        .eq(index)
+        .attr('src', `static/images/media-img${index + 1}-hover.png`);
+    tabIndex = index;
+    mediaLeftContent
+        .find('.media-left-list')
+        .eq(index)
+        .removeClass('hide')
+        .siblings()
+        .addClass('hide');
 });
